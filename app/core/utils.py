da0781326810e1f40b7bbb20f8a3af3f43b42f98
EnetzDev <enetzquindimil@gmail.com>
2025-10-21 13:52:12 +0200

    Bump version to 0.0.8

--- app/core/utils.py
+++ app/core/utils.py
@@ -1,16 +1,40 @@
-import os
+import toml
+from pathlib import Path
 
 def resource_path(filename: str) -> str:
     """
     Returns the absolute path to a file in app/resources
     """
     # Directory of this utils.py file
-    current_dir = os.path.dirname(os.path.abspath(__file__))
+    current_dir = Path(__file__).resolve().parent
 
-    # Go up one level to 'app'
-    app_dir = os.path.abspath(os.path.join(current_dir, ".."))
+    # Go up one level to 'app' and into 'resources'
+    resources_dir = current_dir.parent / "resources"
 
-    # Path to resources folder
-    resources_dir = os.path.join(app_dir, "resources")
+    return str(resources_dir / filename)
 
-    return os.path.join(resources_dir, filename)
+def get_version() -> str:
+    """
+    Returns the version from pyproject.toml located two levels above this utils file.
+    """
+
+    # Directory of this utils.py file
+    current_dir = Path(__file__).resolve().parent
+
+    # Go up two levels to the project root
+    project_root = current_dir.parent.parent
+
+    # Path to pyproject.toml
+    pyproject_path = project_root / "pyproject.toml"
+
+    # Read and parse the TOML file
+    try:
+        pyproject = toml.load(pyproject_path)
+
+        return pyproject["tool"]["poetry"]["version"]
+    
+    except FileNotFoundError:
+        raise FileNotFoundError(f"pyproject.toml not found at {pyproject_path}")
+    
+    except KeyError:
+        raise KeyError("Version key not found in pyproject.toml")
--- app/gui/ui/start_screen_ui.py
+++ app/gui/ui/start_screen_ui.py
@@ -1,4 +1,7 @@
-from PyQt6.QtWidgets import QPushButton, QWidget, QVBoxLayout
+from PyQt6.QtWidgets import QPushButton, QWidget, QVBoxLayout, QLabel
+from PyQt6.QtGui import QFont
+
+from app.core.utils import get_version
 
 class StartScreenUI:
     def setup_ui(self, window):
@@ -10,6 +13,15 @@ class StartScreenUI:
         # Layout
         layout = QVBoxLayout()
         central_widget.setLayout(layout)
+
+        # Version Label
+        version_text_font = QFont()
+        version_text_font.setPointSize(48)
+
+        self.version_text_label = QLabel("v" + get_version())
+        self.version_text_label.setFont(version_text_font)
+
+        layout.addWidget(self.version_text_label)
 
         # Button
         self.start_button = QPushButton("Click Me")

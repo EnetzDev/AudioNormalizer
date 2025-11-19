cf4b3387c8a7106e39220aa8ed73954c5d958831
EnetzDev <enetzquindimil@gmail.com>
2025-11-19 09:10:54 +0100

    Bump version to 0.1.4

--- app/gui/controllers/start_controller.py
+++ app/gui/controllers/start_controller.py
@@ -1,7 +1,8 @@
 class StartScreenController:
     def __init__(self, ui):
         self.ui = ui
-        self.ui.central_widget.start_button.clicked.connect(self.on_start_clicked)
+        self.ui.central_widget.analyze_button.clicked.connect(self.on_start_clicked)
+        self.ui.central_widget.normalize_button.clicked.connect(self.on_start_clicked)
 
     def on_start_clicked(self):
         print("Start button clicked!")
--- app/gui/ui/start_ui.py
+++ app/gui/ui/start_ui.py
@@ -15,13 +15,17 @@ class CentralWidget(QWidget):
         self.image_label.setScaledContents(True)
 
         # Version text
-        self.version_text_label = QLabel("v0.1.1", self)
+        self.version_text_label = QLabel("v0.1.4", self)
         self.version_text_label.setStyleSheet("background-color: transparent; font-weight: bold;")
         self.version_text_label.setFont(QFont("Arial", 16))
 
-        # Start button
-        self.start_button = QPushButton("Click Me", self)
-        self.start_button.setStyleSheet("background-color: transparent;")
+        # Analyze button
+        self.analyze_button = QPushButton("Analyze Audio", self)
+        self.analyze_button.setStyleSheet("background-color: transparent;")
+
+        # Normalize button
+        self.normalize_button = QPushButton("Normalize Audio", self)
+        self.normalize_button.setStyleSheet("background-color: transparent;")
 
     # Resize event to reposition widgets
     def resizeEvent(self, a0=None):
@@ -35,7 +39,7 @@ class CentralWidget(QWidget):
             (size.height() - img_height) // 2
         )
 
-        # Center rotated label
+        # Center label
         self.version_text_label.move(
             int((size.width() - self.version_text_label.width()) / 2),
             int((size.height() - self.version_text_label.height()) / 2 + self.image_label.height() / 8)
@@ -43,8 +47,16 @@ class CentralWidget(QWidget):
 
         # Center button below label
         btn_width, btn_height = 200, 50
-        self.start_button.setGeometry(
-            (size.width() - btn_width) // 2,
+        self.analyze_button.setGeometry(
+            (size.width() - btn_width) // 2 - 100,
+            (size.height() // 2) + 120,
+            btn_width,
+            btn_height
+        )
+
+        # Center button below label
+        self.normalize_button.setGeometry(
+            (size.width() - btn_width) // 2 + 100,
             (size.height() // 2) + 120,
             btn_width,
             btn_height
